769df66f4edb0665c0b1a311d893856f2718b2ad
Tracy Gipson <tracygipson@users.noreply.github.com>
2017-12-09 00:18:01 +0530

    Added a new function! :)

--- app/src/main/java/com/thefantasy/tracy/listdemo/MainActivity.java
+++ app/src/main/java/com/thefantasy/tracy/listdemo/MainActivity.java
@@ -1,7 +1,10 @@
 package com.thefantasy.tracy.listdemo;
 
+import android.app.ListActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Layout;
+import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
@@ -10,21 +13,22 @@ import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.Unbinder;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends ListActivity {
     @BindView(R.id.desc)
     TextView jdesc;
-    @BindView(R.id.title)
-    TextView jtitle;
     @BindView(R.id.img)
     TextView jimg;
-    @BindView(R.id.listy)
-    ListView jlisty;
+    public String[] StringArray = {"tracy","gipson"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
+        TextView jtitle = (TextView) findViewById(R.id.title);
+        ListView jlisty = (ListView) findViewById(R.id.listy);
+        ArrayAdapter adapter = new ArrayAdapter<String>(this,R.layout.single_row,R.id.showTitle, StringArray);
+        jlisty.setAdapter(adapter);
 
     }
 
